fe979ac46f9bfd5bd979bda5a118428efa18ee9d
garytate123 <garytatelufc@gmail.com>
2021-09-17 12:08:38 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+// Copyright year 
+let copyrightYear = new Date();
+copyrightYear = copyrightYear.getFullYear();
+document.getElementById("copyrightYear").innerHTML = " Copyright &copy " + copyrightYear + ": Created by Gary Tate";
